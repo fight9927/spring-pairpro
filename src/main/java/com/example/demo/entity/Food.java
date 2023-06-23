a766d4729f1c62179977164577f9c4c4c525b523
id <id@AQ-0040>
2023-06-24 08:54:10 +0900

    Entityオネシャス

--- src/main/java/com/example/demo/entity/Food.java
+++ src/main/java/com/example/demo/entity/Food.java
@@ -1,12 +1,5 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.Data;
-
-@Entity
-@Data
-@Table(name = "foods")
 public class Food {
 	
 	
