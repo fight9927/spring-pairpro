5492e96156133f74873b7805adcfc289c8d93014
id <id@AQ-0033>
2023-06-26 16:39:39 +0900

    メニュー選択

---
+++ src/main/java/com/example/demo/controller/ChoiceController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.Food;
+import com.example.demo.repository.CategoryRepository;
+import com.example.demo.repository.FoodRepository;
+
+@Controller
+public class ChoiceController {
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+
+	@Autowired
+	FoodRepository foodRepository;
+	
+	// 商品一覧表示
+		@GetMapping("/choice")
+		public String index(
+				@RequestParam(name="categoryId", required=false) Integer categoryId,
+				Model m
+		) {
+			List<Food> foods = foodRepository.findByCategoryId(categoryId);
+			
+			m.addAttribute("foods",foods);
+			
+			return "choice";
+		}
+
+}
--- src/main/java/com/example/demo/repository/FoodRepository.java
+++ src/main/java/com/example/demo/repository/FoodRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Food;
 
 public interface FoodRepository extends JpaRepository<Food, Integer> {
-
+	// SELECT * FROM items WHERE category_id = ?
+		List<Food> findByCategoryId(Integer categoryId);
 }
