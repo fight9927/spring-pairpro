12398a40ddd78e8593644d28e7862f6923f58f9d
id <id@AQ-0040>
2023-06-26 09:52:47 +0900

    RecordからHistory

--- src/main/java/com/example/demo/entity/History.java
+++ src/main/java/com/example/demo/entity/History.java
@@ -1,22 +1,22 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-@Data
+
 @Entity
+@Data
 @Table(name = "records")
-public class Record {
-	//フィールド
+public class History {
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
-	@Column(name = "user_id")
+//	@Column(name = "user_id")
 	private Integer userId;
 	
 	private String name;
--- src/main/java/com/example/demo/repository/RecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecordRepository extends JpaRepository<Record, Integer> {
-
-}
