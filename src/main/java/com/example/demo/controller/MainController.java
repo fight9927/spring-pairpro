ed0b9e0558f64998c935710fc561979280ff7726
id <id@AQ-0033>
2023-06-26 10:24:43 +0900

    w

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,5 +1,34 @@
 package com.example.demo.controller;
 
-public class MainController {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Controller
+public class MainController {
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	User user;
+	
+	@PostMapping("/login")
+	public String login(
+	@RequestParam(value = "name", defaultValue = "") String name,
+	@RequestParam(value = "email", defaultValue = "") String email,
+	@RequestParam(value = "password", defaultValue = "") String password,
+	Model model) {
+		
+		Optional<User> user = userRepository.findByNameAndEmailAndPassword(name,email,password);
+		
+		return "/main";
+	}
+	
+	
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.User;
 
 public interface UserRepository extends JpaRepository <User, Integer> {
+	Optional<User> findByNameAndEmailAndPassword(String name, String email,String password);
 
 }
