6abc494805e1b05fbf7a097b9e8c95302964d105
id <id@AQ-0040>
2023-06-28 16:35:57 +0900

    もう終わりだ

--- src/main/java/com/example/demo/repository/HistoryRepository.java
+++ src/main/java/com/example/demo/repository/HistoryRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,13 @@ import com.example.demo.entity.History;
 public interface HistoryRepository extends JpaRepository<History, Integer> {
 	
 	List<History> findByUserId(Integer userId);
+	
+	List<History> findByUserIdAndDay(Integer userId, LocalDate day);
+	
+	//日付ごとにグループ化してそれを表示させたいんじゃ！！
+//	@Query(
+//			value = "SELECT DATE_FORMAT('day', '%y-%m-%d'), name FROM 'records' GROUP BY day",
+//			nativeQuery=true)
+//	List<Customer> find01(Integer age);
+
 }
