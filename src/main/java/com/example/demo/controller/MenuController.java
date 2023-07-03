46f1bb4d638b091890a61546ec3915581059c688
id <id@AQ-0040>
2023-07-03 11:56:28 +0900

    Merge branch 'master' into sss

--- src/main/java/com/example/demo/controller/MenuController.java
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -123,14 +124,77 @@ public class MenuController {
 		@PostMapping("/menu/add")
 		public String store(
 				@RequestParam(value = "name", defaultValue = "") String name,
-				@RequestParam(value = "carbohydrates", defaultValue = "") Integer carbohydrates,
-				@RequestParam(value = "protein", defaultValue = "") Integer protein,
-				@RequestParam(value = "lipid", defaultValue = "")  Integer lipid,
-				@RequestParam(value = "vitamin", defaultValue = "") Integer vitamin,
-				@RequestParam(value = "mineral", defaultValue = "") Integer mineral,
+				@RequestParam(value = "carbohydrates", defaultValue = "") String carbohydrates,
+				@RequestParam(value = "protein", defaultValue = "") String protein,
+				@RequestParam(value = "lipid", defaultValue = "")  String lipid,
+				@RequestParam(value = "vitamin", defaultValue = "") String vitamin,
+				@RequestParam(value = "mineral", defaultValue = "") String mineral,
 				Model model) {
+			 
+			boolean res = true;
+			
+			String message1 = "";
+			String message2 = "";
+			
+			//名前が正しく入力されているか判定
+			for (int i = 0; i < name.length(); i++) {
+				
+				if (Character.isLetter(name.charAt(i))) {
+					continue;
+				} else {
+					res = false;
+					break;
+				}
+			} 
+			
+			if (res == false) {
+				message1 = "文字のみで入力してください";
+			}
+			
+			//名前が正しく入力されているか判定
+			String pattern = "^[0-9]$";
+			Pattern p1 = Pattern.compile(pattern);
+			
+			if (!(p1.matcher(carbohydrates).find())) {
+				
+				message2 = "栄養素は数字のみで入力してください";
+			}
+			
+            if (!(p1.matcher(protein).find())) {
+ 				
+				message2 = "栄養素は数字のみで入力してください";
+			}
+            
+            if (!(p1.matcher(lipid).find())) {
+				
+				message2 = "栄養素は数字のみで入力してください";
+			}
+            
+            if (!(p1.matcher(vitamin).find())) {
+				
+				message2 = "栄養素は数字のみで入力してください";
+			}
+            
+            if (!(p1.matcher(mineral).find())) {
+				
+				message2 = "栄養素は数字のみで入力してください";
+			}
+            
+            if (!(message1 == "") || !(message2 == "")) {
+            	
+            	model.addAttribute("message1", message1);
+            	model.addAttribute("message2", message2);
+            	model.addAttribute("name", name);
+            	model.addAttribute("carbohydrates", carbohydrates);
+            	model.addAttribute("protein", protein);
+            	model.addAttribute("lipid", lipid);
+            	model.addAttribute("vitamin", vitamin);
+            	model.addAttribute("mineral", mineral);
+            	
+            	return "menu";
+            }
 
-			Food food = new Food(4, name, carbohydrates, protein, lipid, vitamin, mineral);
+			Food food = new Food(4, name, Integer.parseInt(carbohydrates), Integer.parseInt(protein), Integer.parseInt(lipid), Integer.parseInt(vitamin), Integer.parseInt(mineral));
 			
 			 foodRepository.save(food);
 
