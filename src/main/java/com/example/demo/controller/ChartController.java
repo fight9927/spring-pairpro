13496a4a7c0b330ecab74cb82ef798d77a5b17ad
id <id@AQ-0040>
2023-07-04 09:10:30 +0900

    グラフ

--- src/main/java/com/example/demo/controller/ChartController.java
+++ src/main/java/com/example/demo/controller/ChartController.java
@@ -5,8 +5,9 @@ import java.io.IOException;
 import java.util.Base64;
 
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtils;
+import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.StandardChartTheme;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.springframework.stereotype.Controller;
@@ -54,7 +55,8 @@ public class ChartController {
 
         try {
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(); // 画像の出力先
-            ChartUtils.writeChartAsPNG(byteArrayOutputStream, chart, 600, 400); // チャートをPNG画像として出力
+            //ChartUtils.writeChartAsPNG(byteArrayOutputStream, chart, 600, 400); // チャートをPNG画像として出力
+            ChartUtilities.writeChartAsPNG(byteArrayOutputStream, chart, 600, 400); // チャートをPNG画像として出力
             String base64string = Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray()); // 画像をBase64でエンコード
             String dataUri = "data:image/png;base64," + base64string; // data URIの文字列を作成
             modelMap.addAttribute("dataUri", dataUri);
@@ -66,6 +68,8 @@ public class ChartController {
     
     @GetMapping("/graph")
     public String graph(ModelMap modelMap) {
+    	
+    	ChartFactory.setChartTheme(StandardChartTheme.createLegacyTheme());
         // データセットを作成
         DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
@@ -88,7 +92,8 @@ public class ChartController {
 
         try {
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(); // 画像の出力先
-            ChartUtils.writeChartAsPNG(byteArrayOutputStream, chart, 600, 400); // チャートをPNG画像として出力
+            //ChartUtils.writeChartAsPNG(byteArrayOutputStream, chart, 600, 400); // チャートをPNG画像として出力
+            ChartUtilities.writeChartAsPNG(byteArrayOutputStream, chart, 600, 400); // チャートをPNG画像として出力
             String base64string = Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray()); // 画像をBase64でエンコード
             String dataUri = "data:image/png;base64," + base64string; // data URIの文字列を作成
             modelMap.addAttribute("dataUri", dataUri);
